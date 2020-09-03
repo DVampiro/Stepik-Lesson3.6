4fd0ce9f9c679bcb248e9f4a9516624390764837
DVampiro <70638333+DVampiro@users.noreply.github.com>
2020-09-04 01:03:10 +0500

    rename pause_time

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_find_button_and_select_language(browser, pause_select):
+def test_find_button_and_select_language(browser, pause_time):
     browser.get(link)
     browser.implicitly_wait(5)  # На случай задержек
     button = browser.find_elements_by_class_name("btn-add-to-basket")
@@ -15,8 +15,8 @@ def test_find_button_and_select_language(browser, pause_select):
 
     print(f'Выбран язык: {lang}\nТекст на кнопке: {button[0].text}')
 
-    if button[0].text == 'Ajouter au panier' or pause_select:
-        if not pause_select:
-            pause_select = 10  # Принудительное включение для французского
-        print(f'Включена автопауза на {pause_select} секунд')
-        time.sleep(int(pause_select))
+    if button[0].text == 'Ajouter au panier' or pause_time:
+        if not pause_time:
+            pause_time = 10  # Принудительное включение для французского
+        print(f'Включена автопауза на {pause_time} секунд')
+        time.sleep(int(pause_time))
