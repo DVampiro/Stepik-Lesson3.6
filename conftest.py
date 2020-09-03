487aafa5006e9abc33a8317f218e7057a4c59ad7
DVampiro <70638333+DVampiro@users.noreply.github.com>
2020-09-03 23:43:57 +0500

    Добавлены необходимые файлы

---
+++ conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: ru, en, es, fr")
+    parser.addoption('--pause', action='store', default=0,
+                     help="Включение паузы после загрузки")
+
+
+# Включение паузы через командную строку
+@pytest.fixture(scope="function")
+def pause_select(request):
+    pause_mode = request.config.getoption("pause")
+    return pause_mode
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    print("\nstart browser for test..")
+    options = webdriver.ChromeOptions()
+    user_language = request.config.getoption("language")
+    options.add_experimental_option(
+        'prefs', {'intl.accept_languages': user_language})
+
+    # Для отключения всяких сообщений DevTools. Надоели.
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
---
+++ test_items.py
@@ -0,0 +1,20 @@
+import time
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_select_language(browser, pause_select):
+    browser.get(link)
+    browser.implicitly_wait(5)  # На случай задержек
+    button = browser.find_elements_by_class_name("btn-add-to-basket")
+    assert len(button), "Кнопка не найдена!"
+
+    lang = browser.find_element_by_css_selector(
+        "[selected='selected']").text.strip()  # Получение активного языка
+
+    print(f'Выбран язык: {lang}\nТекст на кнопке: {button[0].text}')
+
+    if button[0].text == 'Ajouter au panier' or pause_select:
+        print(f'Включена автопауза на {pause_select} секунд')
+        time.sleep(int(pause_select))
